bad550398eff8baec1f483a67b9669fc4db3dc9b
MustafaLaxmidhar <mufasalaxmdihar@gmail.com>
2024-04-26 17:24:57 +1200

    Initial Commit

---
+++ src/Sonar.cpp
@@ -0,0 +1,35 @@
+#include "Sonar.h"
+#include <Arduino.h>
+
+// USS Setup
+#define TRIG_PIN 48
+#define ECHO_PIN 49
+float sonar_dist = 0;
+
+float measureSonar() {  // returns raw sonar distance
+
+  // Using USS to measure x position
+  long startTime = 0;
+  long endTime = 0;
+
+jump:
+  // Trigger ultrasonic signal
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Measure the time for the signal to return
+  while (digitalRead(ECHO_PIN) == LOW) { startTime = micros(); }
+
+  while (digitalRead(ECHO_PIN) == HIGH) { endTime = micros(); }
+
+  // Calculate distance in milliimeters
+  float pulseDuration = endTime - startTime;
+  sonar_dist = pulseDuration / 5.8;
+
+  delay(1);
+
+  return sonar_dist;
+}   
---
+++ src/Sonar.h
@@ -0,0 +1,6 @@
+#ifndef SONAR_H_
+#define SONAR_H_
+
+float measure_sonar();
+
+#endif
