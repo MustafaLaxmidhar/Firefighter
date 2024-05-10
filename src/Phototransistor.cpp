8da4170bb1e3f8d3b90027e8d2d2e16386bb2677
sdes117 <109499238+sdes117@users.noreply.github.com>
2024-05-10 16:49:17 +1200

    Update Phototransistor.cpp

--- src/Phototransistor.cpp
+++ src/Phototransistor.cpp
@@ -1,1 +1,25 @@
 #include "Phototransistor.h"
+
+int voltGrey(){
+  analogRead(whatever pin);
+}
+
+float distGrey();
+
+int voltPurple(){
+  analogRead(whatever pin);
+}
+
+float distPurple();
+
+int voltGreen(){
+  analogRead(whatever pin);
+}
+
+float distGreen();
+
+int voltBlue(){
+  analogRead(whatever pin);
+}
+
+float distBlue();
