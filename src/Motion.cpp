05a13b2363f4a950a1d9676f6d26b51d64d2a557
sdes117 <109499238+sdes117@users.noreply.github.com>
2024-05-10 16:28:06 +1200

    Motion.cpp
    Bubble Sort Implemented

--- src/Motion.cpp
+++ src/Motion.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 // define motions states 
 enum MOTION{
 FORWARD,
@@ -9,6 +10,20 @@ RIGHT_ARC,
 BACKWARD_LEFT_TURN,
 STOP
 };
+
+void bubbleSort(std::vector<int> &arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 
 // cruise function output command and flag
 void cruise()
