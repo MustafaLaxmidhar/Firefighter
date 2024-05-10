f9233fd67d5fa69eac85d0d111ac8347e11b9d34
sdes117 <109499238+sdes117@users.noreply.github.com>
2024-05-10 16:59:06 +1200

    Update Phototransistor.h

--- include/Phototransistor.h
+++ include/Phototransistor.h
@@ -2,12 +2,8 @@
 #define PHOTOTRANS_H_
 
 int voltGrey();
-float distGrey();
 int voltPurple();
-float distPurple();
 int voltGreen();
-float distGreen();
 int voltBlue();
-float distBlue();
 
 #endif
